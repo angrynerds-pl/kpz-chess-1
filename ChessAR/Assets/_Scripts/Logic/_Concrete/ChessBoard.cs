44b50ab90db1c081c83b40faea6e51615e7f1723
Alex <aleksei.tokareff@gmail.com>
2021-04-08 09:51:39 +0200

    Changed StateMachineInstaller.cs so enemy is starting first. Added if branch in ChessBoard.cs to check the size of a boardStory and decide what firgure should go first

--- ChessAR/Assets/_Scripts/Logic/_Concrete/ChessBoard.cs
+++ ChessAR/Assets/_Scripts/Logic/_Concrete/ChessBoard.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:138aae9860a65e0066c40320f83feae76e59e80f854c6dd3ed4e5c612ee05252
-size 41932
+oid sha256:2c54371fc70f21f2fbda2349084519baa17c5029c7793a45fae8cd1ee1defc40
+size 42404
