f0d54fb877ca615e06b762d7c2e474a2c2cbf16b
Alex <aleksei.tokareff@gmail.com>
2021-04-13 20:40:14 +0200

    Created coroutines for waiting until figure is moved.

--- ChessAR/Assets/_Scripts/Opponent/_Concrete/ComputerAIMoveManager.cs
+++ ChessAR/Assets/_Scripts/Opponent/_Concrete/ComputerAIMoveManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ac9dd6f150892e9a8fa284963bd5a59a4fd67f1ae97389084ce3ce6f007ca64
-size 2576
+oid sha256:cc567cd8b4f71411a0eff779d56b09c1e1178e5e254c67686849a92efce7e254
+size 3216
--- ChessAR/Assets/_Scripts/Player/_Concrete/PlayerMoveManager.cs
+++ ChessAR/Assets/_Scripts/Player/_Concrete/PlayerMoveManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24935db6b5fd19f4a9ade86961d6f9c8cbe0745a1f1870833e8249765fa0d44f
-size 4085
+oid sha256:9ff9fc88effced84e2ac2d9ff60278c1cde0156d15dfd4dae49a440fe077d26e
+size 4529
