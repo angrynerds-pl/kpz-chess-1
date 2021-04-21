3501b21eb4cef10fa310c51c6a0e6e4110db5ccb
Alex <aleksei.tokareff@gmail.com>
2021-04-21 15:46:44 +0200

    Fixed ChessMoveLogic.cs, added mates, peasants etc.

--- ChessAR/Assets/_Scripts/Logic/_Concrete/ChessMoveLogic.cs
+++ ChessAR/Assets/_Scripts/Logic/_Concrete/ChessMoveLogic.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9eee0de26f9d6a82c2d2bf9d5fd1ab63e1dcaf04cd3424d449b28abdc29473f5
-size 29486
+oid sha256:2fa35f2fb83cb14d950c23780467ffcade5779700289ea122f25999c7329acd4
+size 29701
