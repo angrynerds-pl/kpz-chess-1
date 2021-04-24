2afb050e00d343b7e58813bca3fede06721e6865
Alex <aleksei.tokareff@gmail.com>
2021-04-24 16:46:32 +0200

    Removed bug: when figure is moving it is still accessible for choosing

--- ChessAR/Assets/_Scripts/Player/_Concrete/PlayerMoveManager.cs
+++ ChessAR/Assets/_Scripts/Player/_Concrete/PlayerMoveManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1848378ea943dc9bffb3803e542f980c676c830d152cede6f7a21cdd3933978f
-size 5002
+oid sha256:f9e894cea0d08cceef50311b65035753600073b4c1db35d55f6df58cd003e024
+size 5138
