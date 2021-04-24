b1666f8cdd2beb209eb0c4afe323d66991018b6f
Alex <aleksei.tokareff@gmail.com>
2021-04-24 16:37:57 +0200

    Removed bug with selecting allowed squares after move an "on-the-fly"

--- ChessAR/Assets/_Scripts/Player/_Concrete/PlayerMoveManager.cs
+++ ChessAR/Assets/_Scripts/Player/_Concrete/PlayerMoveManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fbea5df45a526ee507a88db7ecf6491305a6000d2fc87f61dc599994c24039c
-size 4976
+oid sha256:1848378ea943dc9bffb3803e542f980c676c830d152cede6f7a21cdd3933978f
+size 5002
