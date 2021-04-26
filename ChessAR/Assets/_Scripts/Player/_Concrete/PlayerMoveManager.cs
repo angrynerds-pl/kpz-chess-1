ecd4cc1f1ac33deb2eaac4a908c660ff7ad6c623
Alex <aleksei.tokareff@gmail.com>
2021-04-26 20:32:48 +0200

    Fixed castling for white figures. En Passant is commented.

--- ChessAR/Assets/_Scripts/Player/_Concrete/PlayerMoveManager.cs
+++ ChessAR/Assets/_Scripts/Player/_Concrete/PlayerMoveManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9e894cea0d08cceef50311b65035753600073b4c1db35d55f6df58cd003e024
-size 5138
+oid sha256:6c681bb0cb022971bc8308269c711f332a836d3f0896b90fa84137c1ae7decac
+size 5111
---
+++ ChessAR/Assets/_Scripts/SpecialMoves/_Concrete/SpecialMovesHandler.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6da96791fc5791d05fdb93fdbb9fddea81549d2c7945d5238b6a4a448965442b
+size 675
