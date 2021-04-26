d7f8943c17854bf06f70231656ac72a827284341
Alex <aleksei.tokareff@gmail.com>
2021-04-26 21:14:07 +0200

    Fixed castling for black figures

--- ChessAR/Assets/_Scripts/FigureData/_Concrete/PositionData.cs
+++ ChessAR/Assets/_Scripts/FigureData/_Concrete/PositionData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98e29058896661d39f9b60742713b89a8daea1c913a20d558003cc6abcb4a50a
-size 696
+oid sha256:1b0811187fe7c735cc820f7ca14c183ed08150dfed1fa408b4865e6503300265
+size 730
