72f614ae25e92f8a8c471f539d061269bb0a41e4
Alex <aleksei.tokareff@gmail.com>
2021-05-04 20:18:06 +0200

    Added basic animation for castles

--- ChessAR/Assets/_Scripts/SpecialMoves/_Concrete/SpecialMovesHandler.cs
+++ ChessAR/Assets/_Scripts/SpecialMoves/_Concrete/SpecialMovesHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54d2b8087394e5509158d857c3a9f9a3c84cc05657f507e3200aaa6aa0b85462
-size 997
+oid sha256:e9816ce4a0bf5b82e90a19021e65792414eef2971abfb95598e0db6542a5dd5b
+size 1805
