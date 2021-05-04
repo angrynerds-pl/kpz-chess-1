441c884f8e3796da7b8ec970c23461a95e897022
Alex <aleksei.tokareff@gmail.com>
2021-05-04 11:59:37 +0200

    Changed CastlingSignal.cs

--- ChessAR/Assets/_Scripts/Logic/_Concrete/ChessMoveLogic.cs
+++ ChessAR/Assets/_Scripts/Logic/_Concrete/ChessMoveLogic.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb480b57281a42072f07e09d30b110c2a8c5f91d1e552772e5065ed28170fde4
-size 30997
+oid sha256:e70e3442d3b8089db5dae2cd7e347842ea3915d5bed5c8cfd62416a704865b21
+size 31027
--- ChessAR/Assets/_Scripts/SpecialMoves/_Concrete/SpecialMovesHandler.cs
+++ ChessAR/Assets/_Scripts/SpecialMoves/_Concrete/SpecialMovesHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6da96791fc5791d05fdb93fdbb9fddea81549d2c7945d5238b6a4a448965442b
-size 675
+oid sha256:54d2b8087394e5509158d857c3a9f9a3c84cc05657f507e3200aaa6aa0b85462
+size 997
