afd73260892ce6436fae3f999192559e0a789b73
Alex <aleksei.tokareff@gmail.com>
2021-05-05 09:11:56 +0200

    Added EnPassant signal and its handling in SpecialMovesHandler.cs

--- ChessAR/Assets/_Scripts/SpecialMoves/_Concrete/SpecialMovesHandler.cs
+++ ChessAR/Assets/_Scripts/SpecialMoves/_Concrete/SpecialMovesHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0330445f7c47bd4c37c649221b6119b3e0ad283bd9fc9c8455dcb4c3076a25ec
-size 2655
+oid sha256:20a8296ac196c8e6ad82eb78074af4b9f4b6c15a60220fb64671df0375f092af
+size 3241
