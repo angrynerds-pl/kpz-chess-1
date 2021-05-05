154019451f92e96b18b2c0a1d31133df58c534ec
Alex <aleksei.tokareff@gmail.com>
2021-05-05 15:01:30 +0200

    Added CameraAccessor.cs for accessing camera needed for particular situation. Added installer to it.

---
+++ ChessAR/Assets/_Scripts/Cameras/_Contract/ICamerasAccessor.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2e1db30beda1f515672a29853768e5216b6f684878ee5a0f363c22c7fb48c300
+size 130
---
+++ ChessAR/Assets/_Scripts/Cameras/_Installers/CamerasInstaller.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a555517c4ec041b2f87643a7026c05f5cf2d97b5b4117d36be39851128e3e7b2
+size 305
