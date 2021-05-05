f422fca642d598a495e0dc14ffcafbc5c61bf142
Alex <aleksei.tokareff@gmail.com>
2021-05-05 16:54:41 +0200

    Fixed RayCastProvider to choose proper camera

--- ChessAR/Assets/_Scripts/FigureSelection/_Concrete/MouseScreenRayProvider.cs
+++ ChessAR/Assets/_Scripts/FigureSelection/_Concrete/MouseScreenRayProvider.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c4e42beca4c87f510d2133b988248fe9a8f45ef6a263691931c72962ec2b6ed
-size 768
+oid sha256:7b6fcf70ab5c22d26c1f7128af80a894a3148e4c93bd9bcb6987171e8e28e9bf
+size 672
--- ChessAR/Assets/_Scripts/FigureSelection/_Concrete/SelectionManager.cs
+++ ChessAR/Assets/_Scripts/FigureSelection/_Concrete/SelectionManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c87f07c6e1aaf606371365d287c53e86148375e9413961c843450c10eaefc41
-size 3483
+oid sha256:0bfd7412f82424fd822aef4f94c70cf3cb24125d3f78539a0809a6916cfd0f23
+size 3447
