7daf39348a087cda14e3499572738825c524394c
Alex <aleksei.tokareff@gmail.com>
2021-05-07 15:55:10 +0200

    Added licence to vuforia and changed World Center Mode of ARCamera to FIRST_TARGET

--- ChessAR/Assets/_Scripts/FigureSelection/_Concrete/MouseScreenRayProvider.cs
+++ ChessAR/Assets/_Scripts/FigureSelection/_Concrete/MouseScreenRayProvider.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b6fcf70ab5c22d26c1f7128af80a894a3148e4c93bd9bcb6987171e8e28e9bf
-size 672
+oid sha256:363290a1c14fea1af6fe6ec181bc74a44074ae3ffb629b7fdaec4f3602cb054c
+size 745
